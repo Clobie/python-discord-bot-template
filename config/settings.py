0bee35da6d9071ce931f15e57e3e8c2db449b7db
Clobie <calebtalexander@gmail.com>
2024-07-13 13:02:57 -0400

    main.py template

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,12 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-LOGDIR = os.path.join(BASE_DIR, 'logs')
-
+# Discord settings
 BOT_PREFIX = "!"
+
+# DIrectory structure
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DIR_LOGS = os.path.join(BASE_DIR, 'logs')
+DIR_COGS = os.path.join(BASE_DIR, 'bot', 'cogs')
+DIR_UTILS = os.path.join(BASE_DIR, 'bot', 'utils')
+DIR_CONFIG = os.path.join(BASE_DIR, 'config')
+DIR_TESTS = os.path.join(BASE_DIR, 'tests')
